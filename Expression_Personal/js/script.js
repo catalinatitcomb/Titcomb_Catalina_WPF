fffea787bc4804adb5868e56dc3b9b73c4205e47
catalinatitcomb <catalina.titcomb@fullsail.edu>
2013-12-03 22:30:32 -0500

    Changes to console.log, and explanation of variables and prompts

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -3,24 +3,29 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
 
+//Catalina Titcomb
+//December 3, 2013
+//Expressions Personal
+
+
 var credit; //Amount of deposits
 var debit; //Amount of withdrawals
-var interestRate;
-var totalInterest;
-var balance;
+var interestRate; //Amount of Interest Rate charge by the bank
+var totalInterest; //Amount of Interest Rate paid to the bank
+var balance; // Total Balance in Account
 
 
-credit = prompt("How much money do you have at the beginning of the month in your account?");
-debit = prompt("How much did you spend in the month?");
-interestRate = prompt("how much interest does the bank charge (in decimal form)?");
+credit = prompt("How much money do you have at the beginning of the month in your account?"); //Prompt user to enter the beginning balance
+debit = prompt("How much did you spend in the month?"); //Prompt user to enter amount spent during the month
+interestRate = prompt("how much interest does the bank charge (in decimal form)?"); //Prompt user for Bank interest rate
 
-totalInterest = (Number(credit) - Number(debit)) * Number(interestRate);
+totalInterest = (Number(credit) - Number(debit)) * Number(interestRate); //Total interest paid to the bank
 
-balance = Number(credit) - Number(debit) - Number(totalInterest);
+balance = Number(credit) - Number(debit) - Number(totalInterest); //Final amount of money in your account
 
 alert("Your balance at the end of the month is $" + balance + " and you paid $" + totalInterest + " in interest.");
 
-console.log(balance);
+console.log("Your balance at the end of the month is $" + balance + " and you paid $" + totalInterest + " in interest.");
 
 
 
