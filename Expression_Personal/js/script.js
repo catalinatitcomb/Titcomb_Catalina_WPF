f9eba0d332b2b5217db6d717184fddec92f33c62
catalinatitcomb <catalina.titcomb@fullsail.edu>
2013-12-03 22:22:50 -0500

    Changes made to formula, added prompts

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -3,15 +3,24 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
 
-var credit = 1000; //Amount of deposits
-var debit = 345; //Amount of withdrawals
+var credit; //Amount of deposits
+var debit; //Amount of withdrawals
+var interestRate;
+var totalInterest;
 var balance;
-balance = credit - debit; //Difference of credits and debits
 
-var percentage;
-percentage = balance * 0.3; //Multiplying balance by 3% percentage
 
-var totalBalance;
-totalBalance = balance + percentage; //Total balance adding balance plus percentage
+credit = prompt("How much money do you have at the beginning of the month in your account?");
+debit = prompt("How much did you spend in the month?");
+interestRate = prompt("how much interest does the bank charge (in decimal form)?");
 
-console.log("Your balance is $" + " " + totalBalance);
+totalInterest = (Number(credit) - Number(debit)) * Number(interestRate);
+
+balance = Number(credit) - Number(debit) - Number(totalInterest);
+
+alert("Your balance at the end of the month is $" + balance + " and you paid $" + totalInterest + " in interest.");
+
+console.log(balance);
+
+
+
