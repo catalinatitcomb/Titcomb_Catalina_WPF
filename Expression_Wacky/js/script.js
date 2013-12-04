2a3f43f80ddf65aaa20f2a65a7cb40dbd4d68933
catalinatitcomb <catalina.titcomb@fullsail.edu>
2013-12-04 12:57:31 -0500

    Explaining what the variables are

--- Expression_Wacky/js/script.js
+++ Expression_Wacky/js/script.js
@@ -7,11 +7,11 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 //December 3, 2013
 //Expressions Wacky
 
-var check;
-var tax;
-var tip;
-var totalCheck;
-var totalTip;
+var check; //How much you need to pay for what you ate
+var tax; //How much you have to pay in taxes in your state
+var tip; //What percentage of the check you want to pay the waiter
+var totalCheck; //Total amount you have to pay, adding the check amount and the tax amount
+var totalTip; //How much you want to tip the waiter
 
 check = prompt("How much do the bill add?");
 tax = prompt("Enter tax percentage in your state (in decimal form)");
