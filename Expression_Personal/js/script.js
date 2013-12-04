d2656d30cec0f269a510a21df4462e5bbbf47670
catalinatitcomb <catalina.titcomb@fullsail.edu>
2013-12-03 20:58:41 -0500

    Changes to Expression Personal, added comments

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -3,15 +3,15 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
 
-var credit = 1000;
-var debit = 345;
+var credit = 1000; //Amount of deposits
+var debit = 345; //Amount of withdrawals
 var balance;
-balance = credit - debit;
+balance = credit - debit; //Difference of credits and debits
 
 var percentage;
-percentage = balance * 0.3;
+percentage = balance * 0.3; //Multiplying balance by 3% percentage
 
 var totalBalance;
-totalBalance = balance + percentage;
+totalBalance = balance + percentage; //Total balance adding balance plus percentage
 
 console.log("Your balance is $" + " " + totalBalance);
