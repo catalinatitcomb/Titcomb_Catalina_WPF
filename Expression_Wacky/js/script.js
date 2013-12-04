9c00884b5bd2d74258aa82ebd2961f1e7ea93e54
catalinatitcomb <catalina.titcomb@fullsail.edu>
2013-12-04 13:15:13 -0500

    Changes to percentage of the tip

--- Expression_Wacky/js/script.js
+++ Expression_Wacky/js/script.js
@@ -12,6 +12,7 @@ var tax; //How much you have to pay in taxes in your state
 var tip; //What percentage of the check you want to pay the waiter
 var totalCheck; //Total amount you have to pay, adding the check amount and the tax amount
 var totalTip; //How much you want to tip the waiter
+var amountToPay;
 
 check = prompt("How much do the bill add?"); //Prompt user to enter check amount before taxes
 tax = prompt("Enter tax percentage in your state (in decimal form)"); //Prompt user to enter tax amount charge in the state where user lives
@@ -19,9 +20,11 @@ tip = prompt("What percentage do you want to tip the waiter? (in decimal form)?"
 
 totalCheck = (Number(check) * Number(tax)) + Number(check); //Total amount user has to pay
 
-totalTip = Number(totalCheck) * Number(tip); //Total amount of tip user wants to pay to waiter
+totalTip = Number(check) * Number(tip); //Total amount of tip user wants to pay to waiter
 
-alert("You need to pay $" + totalCheck + " and the tip for the waiter is $ " + totalTip); //Window telling user how much user needs to pay total, adding total check and total tip
+amountToPay = Number(totalCheck) + Number(totalTip);
+
+alert("The total of your check is $" + totalCheck + " and the tip for the waiter is $ " + totalTip); //Window telling user how much user needs to pay total, adding total check and total tip
 
 console.log("You need to pay $" + totalCheck + " and the tip for the waiter is $ " + totalTip);
 
