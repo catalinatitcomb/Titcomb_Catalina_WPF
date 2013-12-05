318bbec17807a07d0374c4e6160f66f10a25d5fa
catalinatitcomb <catalina.titcomb@fullsail.edu>
2013-12-04 22:35:49 -0500

    Add variables names

--- Expression_Industry/js/script.js
+++ Expression_Industry/js/script.js
@@ -5,3 +5,10 @@
 //Divine Proportion
 //it is not only to have beautiful colors in a design, one of the most important facts in a design is that it is well balanced.
 //The Divine Proportion helps us to create a well balanced design, and this makes that design look wonderful.
+
+var width;
+var proportion = 1.62;
+var sideBar;
+var divineProportion;
+
+
