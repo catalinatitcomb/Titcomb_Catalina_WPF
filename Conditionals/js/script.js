1536b5347a74bdda30be45ba12897dcb08eb6cdc
catalinatitcomb <catalina.titcomb@fullsail.edu>
2013-12-10 13:36:20 -0500

    Console Log

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -3,5 +3,5 @@
  //if the child os old enough, print to the console "You can ride!"
  if(oldEnough){
      //code performed if condition is true
-     
+     console.log("You can ride the coaster!");
  }
